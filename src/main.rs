99a71093eb4a19d5c86fe10d1abaacf5f7b275a7
sean <pulpfictionost@gmail.com>
2021-02-16 16:41:37 +0000

    Create Challenge Accept Payload

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use native_tls::TlsConnector;
 use native_tls::TlsStream;
 use serde_json::Value;
 use std::io::BufRead;
-use std::io::BufReader;
+use bufstream::BufStream;
 use std::io::Write;
 use std::net::TcpStream;
 
@@ -13,44 +13,35 @@ fn try_parse_json(json_string: &str) -> serde_json::Result<Value> {
 
 // Accept Challenge
 
-// > POST /api/challenge/MUlGTvsX/accept HTTP/1.1
-// > Host: lichess.org
-// > User-Agent: curl/7.68.0
-// > Accept: */*
-// > Authorization: Bearer {}
-// > 
-
-
 fn connect_tls_stream() -> Result<TlsStream<TcpStream>, String> {
     println!("TLS connect starting");
-    let lichess_api_token = std::env::var("lichess_api_token").unwrap_or("NA".to_string());
-    let stream_event_msg = format!(
-        "GET /api/stream/event HTTP/1.1\nHost: lichess.org\nUser-Agent: curl/7.68.0\nAccept: */*\nAuthorization: Bearer {}\n\n", 
-    lichess_api_token);
-
-    println!("{}", stream_event_msg);
-
     let connector = TlsConnector::new().unwrap();
-
     let tcp_stream = match TcpStream::connect("lichess.org:443".to_owned()) {
         Ok(stream) => stream,
         Err(e) => return Err(e.to_string()),
     };
-    let mut tls_stream = match connector.connect(&"lichess.org".to_owned(), tcp_stream) {
+    let tls_stream = match connector.connect(&"lichess.org".to_owned(), tcp_stream) {
         Ok(stream) => stream,
         Err(e) => return Err(e.to_string()),
     };
-
-    tls_stream.write_all(stream_event_msg.as_bytes()).unwrap();
     return Ok(tls_stream);
 }
 
-pub async fn subscribe() {
-    let stream = connect_tls_stream().unwrap();
+fn send_msg(tls_stream: &mut TlsStream<TcpStream>, stream_event_msg: &String) {
+    tls_stream.write_all(stream_event_msg.as_bytes()).unwrap();
+}
 
-    let mut stream_reader = BufReader::new(stream);
+async fn subscribe() {
+    let mut stream = connect_tls_stream().unwrap();
+    let lichess_api_token = std::env::var("lichess_api_token").unwrap_or("NA".to_string());
+    let stream_event_msg = format!(
+        "GET /api/stream/event HTTP/1.1\nHost: lichess.org\nUser-Agent: curl/7.68.0\nAccept: */*\nAuthorization: Bearer {}\n\n", 
+    lichess_api_token);
+    send_msg(&mut stream, &stream_event_msg);
+
+    let mut stream = BufStream::new(&mut stream);
     let mut buf = String::new();
-    while stream_reader.read_line(&mut buf).unwrap_or(0) > 0 {
+    while stream.read_line(&mut buf).unwrap_or(0) > 0 {
         println!("{}", &buf);
         match try_parse_json(&buf) {
             Ok(v) => {
@@ -59,6 +50,12 @@ pub async fn subscribe() {
                 match msg_type.as_ref() {
                     r#""challenge""# => {
                         println!("Challenge Message Type");
+                        let challenge_id = v["challenge"]["id"].as_str().unwrap().to_owned();
+                        let accept_challenge_msg = format!(
+                            "POST /api/challenge/{}/accept HTTP/1.1\nHost: lichess.org\nUser-Agent: curl/7.68.0\nAccept: */*\nAuthorization: Bearer {}\n\n", 
+                        challenge_id, lichess_api_token);
+                        println!("Challenge Message Type: \n{}", accept_challenge_msg);
+                        // send_msg(&stream, &accept_challenge_msg);
                     }
                     _ => {
                         println!("Unknown Message Type: {}", msg_type);
